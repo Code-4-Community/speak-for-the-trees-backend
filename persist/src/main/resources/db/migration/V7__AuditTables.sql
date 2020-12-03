30644aab2827a1dca5c6725dcf5be112e1e71731
BreeMadison <breannamadison711@gmail.com>
2020-12-03 12:56:26 -0500

    Making result column not null

--- persist/src/main/resources/db/migration/V7__AuditTables.sql
+++ persist/src/main/resources/db/migration/V7__AuditTables.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS audit
     transaction_type VARCHAR(10)   NOT NULL,
     table_name       VARCHAR(10)   NOT NULL,
     user_id          INTEGER       NOT NULL,
-    old_value        VARCHAR(1000) NOT NULL,
+    old_value        VARCHAR(1000),
     result           VARCHAR(1000) NOT NULL,
     timestamp        TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
